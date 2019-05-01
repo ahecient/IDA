105e10fc98632f6b2a94c03b29f7cb3a4ecd8eec
ahecient <48700087+ahecient@users.noreply.github.com>
2019-05-01 17:40:37 +0300

    entropy purty gain

--- Lab3/3.R
+++ Lab3/3.R
@@ -1,19 +1,25 @@
 library(tm)
 
-Entropy_class <- function(table){
-  Entropy <- integer()
-  for(i in 1: length(table)){
-    p <- table[i]/sum(table)
-    Entropy <- append(Entropy, -(p*log2(p)))
-  }
-  Entropy <- sum(Entropy)
-  return(Entropy)
+Entropy <- function(x){
+  Entropy <-x/sum(x)*log2(x/sum(x))
+  Entropy[x==0] <- 0
+  -sum(Entropy)
+}
+
+Purity <- function(table){
+  length(unique(table[,ncol(table)]))==1
+}
+
+Gain <- function(table){
+  EntropyBefore <- Entropy(colSums(table))
+  
+  row_sum <- rowSums(table)
+  EntropyAfter <- sum(row_sum/sum(row_sum)*apply(table, MARGIN = 1, FUN = Entropy))
+  IGain <- EntropyBefore - EntropyAfter
+  return(IGain)
 }
 
 Lenses <- read.table("lenses.txt", sep = "\t", stringsAsFactors = FALSE, check.names = FALSE, header = TRUE)
 
-Class_lenses <- table(Lenses$contactlenses)
+if(Purity(Lenses)) print("+") else print("-")
 
-Entropy_class(Class_lenses)
-
-P_table <- data.frame()

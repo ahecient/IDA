c7dd918ba5e637cdd91949fac9bb0478499bc190
ahecient <48700087+ahecient@users.noreply.github.com>
2019-05-01 21:13:24 +0300

    finally + play with visual

--- Lab3/3.R
+++ Lab3/3.R
@@ -1,25 +1,55 @@
 library(tm)
+library(data.tree)
 
-Entropy <- function(x){
-  Entropy <-x/sum(x)*log2(x/sum(x))
-  Entropy[x==0] <- 0
+Entropy <- function(data) {
+  P <- data/sum(data)
+  Entropy <- P * log2(P)
+  Entropy[data == 0] <- 0
   -sum(Entropy)
 }
 
+#"чистота" определяет, один класс или нет
 Purity <- function(table){
   length(unique(table[,ncol(table)]))==1
 }
 
+#мера информационного выиграша определяется как разность 
+#энтропии для исходного набора данных и суммы энтропий всех фрагментов после разбиения
 Gain <- function(table){
   EntropyBefore <- Entropy(colSums(table))
-  
   row_sum <- rowSums(table)
   EntropyAfter <- sum(row_sum/sum(row_sum)*apply(table, MARGIN = 1, FUN = Entropy))
   IGain <- EntropyBefore - EntropyAfter
   return(IGain)
 }
 
-Lenses <- read.table("lenses.txt", sep = "\t", stringsAsFactors = FALSE, check.names = FALSE, header = TRUE)
+ID3 <- function(node, data) {
+  
+  if (Purity(data)) {
+    child <- node$AddChild(unique(data[,ncol(data)]))
+    } 
+  else {
+    ig <- sapply(colnames(data)[-ncol(data)], function(x) Gain(table(data[,x], data[,ncol(data)])))
 
-if(Purity(Lenses)) print("+") else print("-")
+    feature <- names(which.max(ig))
+    childObs <- split(data[ ,names(data) != feature, drop = FALSE], data[ ,feature], drop = TRUE)
+    
+    for(i in 1:length(childObs)) {
+      child <- node$AddChild(names(childObs)[i])
+      ID3(child, childObs[[i]])
+    }
+  }
+}
+
+Lenses <- read.table("lenses.txt", sep = "\t", check.names = FALSE, header = TRUE)
+
+tree <- Node$new("astegmatism")
+
+ID3(tree, Lenses)
+print(tree)
+plot(tree)
+plot(as.dendrogram(tree))
+
+SetEdgeStyle(tree, color = "blue")
+SetNodeStyle(tree, style = "filled, rounded", shape = "box", fillcolor = "red")
 

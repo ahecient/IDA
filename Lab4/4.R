7d0d30c53b91c4379d91cc581c6e7225af48fa56
ahecient <48700087+ahecient@users.noreply.github.com>
2019-05-02 17:25:06 +0300

    finally: simple + multi + different DS ^_^

--- Lab4/4.R
+++ Lab4/4.R
@@ -1,24 +1,47 @@
-#простая линейная регрессия
+#simple for 2 var method least squares
+Reg2varLS <- function(table, y, x){
+  LeastSquares <- table
+  
+  LeastSquares$XSquare <- LeastSquares[,x]^2
+  LeastSquares$XY <- LeastSquares[,x]*LeastSquares[,y]
+  LeastSquares <- rbind(LeastSquares, colSums(LeastSquares))
+  
+  A <- array(c(LeastSquares$XSquare[nrow(LeastSquares)], LeastSquares[nrow(LeastSquares),x],
+               LeastSquares[nrow(LeastSquares),x], nrow(LeastSquares)-1 ), c(2,2))
+  b <- c(LeastSquares$XY[nrow(LeastSquares)], LeastSquares[nrow(LeastSquares),y])
+  
+  Coef <- solve(A,b)
+  return(Coef)
+}
 
+#multi for 1:n
+MultipleRegression <- function(table, prediction){
+  Y <- table[,prediction]
+  Intercept <- rep(1, nrow(table))
+  table <- table[,-c(prediction)]
+  table <- cbind(table, Intercept)
+  X <- as.matrix(table)
+  Xt <- t(X)
+  XtX <- Xt %*% X
+  XtY <- Xt %*% Y
+  ReverseXtX <- solve(XtX)
+  S = ReverseXtX %*% XtY
+  return(S)
+}
+
+
+#######TEST#######
+
+#simple
 data(cars)
 scatter.smooth(x=cars$speed, y = cars$dist, main = "Dist ~ Speed")
 
 linearMod <- lm(dist ~ speed, data = cars)
 summary(linearMod)
+Reg2varLS(cars, 2, 1)
 
-LeastSquares <- cars
 
-LeastSquares$speedSquare <- LeastSquares$speed^2
-LeastSquares$speedXdist <- LeastSquares$speed*LeastSquares$dist
-LeastSquares <- rbind(LeastSquares, colSums(LeastSquares))
-
-A <- array(c(LeastSquares$speedSquare[nrow(LeastSquares)], LeastSquares$speed[nrow(LeastSquares)],
-             LeastSquares$speed[nrow(LeastSquares)], nrow(LeastSquares)-1 ), c(2,2))
-b <- c(LeastSquares$speedXdist[nrow(LeastSquares)], LeastSquares$dist[nrow(LeastSquares)])
-
-Coef <- solve(A,b)
-
-#множественная регрессия
+#multi
 library(car)
 long <- data.frame(longley)
 cor(long)
@@ -26,6 +49,19 @@ scatterplotMatrix(long, main = "Соотношение переменных")
 
 model <- lm(Population ~., data = long)
 summary(model)
+
+MultipleRegression(long, 5)
+
+################
+
+data(trees)
+cor(trees)
+scatterplotMatrix(trees, main = "Соотношение переменных")
+
+linmodel <- lm(Height ~., data = trees)
+summary(linmodel)
+
+MultipleRegression(trees, 2)
 
 #Intercept -  Если у нас модель представлена в виде уравнения, то тогда б0 - точка пересечения прямой с точкой координат
 
@@ -45,3 +81,5 @@ summary(model)
 
 #p value — Это вероятность истинности нуль гипотезы, которая гласит, что независимые переменные не объясняют динамику зависимой переменной. 
 #Если значение p value ниже порогового уровня (.05 или .01 для самых взыскательных), то нуль гипотеза ложная. Чем ниже — тем лучше.
+
+
